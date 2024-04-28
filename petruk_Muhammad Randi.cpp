d3cc75c34f656d6c9fb87db357d72cf751a1f90c
Muhammad Randi Putra Kurniawan <151342274+randipkur09@users.noreply.github.com>
2024-04-28 16:40:13 +0700

    Update petruk_Muhammad Randi.cpp

--- petruk_Muhammad Randi.cpp
+++ petruk_Muhammad Randi.cpp
@@ -6,14 +6,15 @@ using namespace std;
 
 //Muhammad Randi Putra Kurniawan
 //2317051009
-bool searchWord(vector<string> matrix, string word) {
-    int rows = matrix.size();
-    int cols = matrix[0].size();
+bool cariKata(vector<string> matriks, string kata) {
+    int baris = matriks.size();
+    int kolom = matriks[0].size();
 
-    // nyari horizontal
-    for (int i = 0; i < rows; i++) {
-        if (matrix[i].find(word) != string::npos) {
+    // Mencari secara horizontal
+    for (int i = 0; i < baris; i++) {
+        if (matriks[i].find(kata) != string::npos) {
             return 1;
         }
     }
+
 
